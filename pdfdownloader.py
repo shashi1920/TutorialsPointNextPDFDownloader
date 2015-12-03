4edbc9a978e22c67450dd67524e4c24dfb493a29
shashi1920 <shashikant.sharma.cse12@iitbhu.ac.in>
2015-12-04 01:39:15 +0530

    Bug Fixes + More Efficient

--- pdfdownloader.py
+++ pdfdownloader.py
@@ -2,27 +2,44 @@ __author__ = 'Shashi'
 import requests
 import bs4
 import urllib
+import time
+import os
+import wget
 
 
-
-def pdfdownloader(url):
+def pdfdownloader(url,start,seen):
+    if url in seen:
+            return 1
     header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
 
     response = requests.get(url,headers=header)
 
     #print response.text
     download=False
+    next=False
     soup = bs4.BeautifulSoup(response.text)
-    for link in soup.find_all('a'):
+    if soup.findAll("div", { "class" : "pdf-btn" }):
+        for div in soup.findAll("div", { "class" : "pdf-btn" }):
+            div.a["href"].split('/')[-1]=wget.download("http://www.tutorialspoint.com" +div.a["href"])
+            print("Saved: "+div.a["href"].split('/')[-1])
+    else:
+        url.split('/')[-1]=wget.download(url)
+    for div in soup.findAll("div", { "class" : "nxt-btn" })[:1]:
+        print "Working on : " + "http://www.tutorialspoint.com"+div.a["href"]
+        seen.append(url)
+        pdfdownloader("http://www.tutorialspoint.com" +div.a["href"],start,seen)
+    return 1
 
-        if '.pdf' in (link.get('href')):
-            print "Saving now: " + link.get('href').split('/')[-1]
-            testfile = urllib.URLopener()
-            testfile.retrieve("http://www.tutorialspoint.com"+link.get('href'), link.get('href').split('/')[-1])
-            download=True
-        if download:
-            pdfdownloader("http://www.tutorialspoint.com"+link.get('href'))
+
+
 
 
 start=raw_input("Enter the starting URL for the tutorial: ") #Enter the url of starting tutorial
-pdfdownloader(start)
+DIR=' '.join(start.split('/')[3:])
+if not os.path.exists(DIR):
+    os.makedirs(DIR)
+seen=[]
+
+os.chdir(DIR)
+pdfdownloader(start,start,seen)
+print "Successfully Downloaded "+str(len(seen))+" files"
